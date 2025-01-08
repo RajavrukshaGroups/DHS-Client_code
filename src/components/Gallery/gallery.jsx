3d16091cb1076e983e2c6808b77045772214e88e
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-01-08 13:57:50 +0530

    new-photos

--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -7,9 +7,14 @@ import three from '../Project_Images/four.jpg'
 import four from '../Project_Images/five.jpg';
 import five from '../Project_Images/DSC04739.jpg';
 import six from '../Project_Images/six.jpg';
+import seven from "../Project_Images/seven.JPG";
+import eight from '../Project_Images/eight.JPG';
+import nine from '../Project_Images/nine.JPG';
+import ten from '../Project_Images/ten.JPG';
+import eleven from '../Project_Images/eleven.JPG';
+import twelve from "../Project_Images/twelve.JPG";
+import thirteen from "../Project_Images/thirteen.JPG";
 
-// import five from '../Project_Images'
-// import six from '../Project_Images'
 function Gallery() {
   //new galleryy
   return (
@@ -52,7 +57,18 @@ function Gallery() {
           <img src={two} alt="img-two" />
         </div>
         <div className='img-container'>
-          <img src={four} alt="img-three" />
+          <img src={thirteen} alt="img-three" />
+        </div>
+      </div>
+      <div className='containers'>
+        <div className='img-container'>
+          <img src={seven} alt="img-one" />
+        </div>
+        <div className='img-container'>
+          <img src={ten} alt="img-two" />
+        </div>
+        <div className='img-container'>
+          <img src={twelve} alt="img-three" />
         </div>
       </div>
     </div>
